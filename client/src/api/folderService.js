23f187097d94eeacb71a6375481f3b5e173f5c3b
ivanisovich <57730983+ivanisovich@users.noreply.github.com>
2024-04-08 13:51:08 +0500

    commit

--- client/src/api/folderService.js
+++ client/src/api/folderService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://91.223.123.165:3000/folders';
+const baseUrl = '91.223.123.165:3000/folders';
 
 export const fetchFolders = async () => {
   try {
